b4ec19c590b2964c315262809535108ec0d9a5b6
Tom Lee <live4dream@stu.xjtu.edu.cn>
2021-04-19 11:57:39 +0800

    修改数据库; ws初试

---
+++ ws/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+func Echo(ws *websocket.Conn) {
+	var err error
+	var reply string
+	for {
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+
+		msg := "Received:  " + reply
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
+func main() {
+	h := websocket.Handler(Echo)
+	http.Handle("/wsecho", h)
+
+	if err := http.ListenAndServe(":43852", h); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
